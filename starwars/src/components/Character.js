798ef34cad1b33b8ed41bb0bdcf2d040dd140f45
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-06-12 12:54:01 -0700

    Added styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,26 @@
 // Write your Character component here
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
-import { BASE_URL } from '../constants'
+import React from "react";
+// import axios from 'axios'
+// import { BASE_URL } from '../constants'
 import styled from 'styled-components'
+import Container from './Container'
+
+const changes = (color1 = 'yellow', size = '3rem', color2 = 'gold') => {
+    return {
+        h2: {
+            fontSize: { size },
+            color: { color1 },
+        },
+        li: {
+            fontSize: `${size - 1.0}`,
+            color: { color2 },
+        }
+    }
+}
 
 export default function Character(props) {
     console.log('props: ' + JSON.stringify(props));
-    const { starwarsCharacterId } = props
+    // const { starwarsCharacterId } = props
     // const [starwarsCharacter, setStarwarsCharacter] = useState(null)
 
     // useEffect(() => {
@@ -29,17 +43,17 @@ export default function Character(props) {
     console.log('displayCharacters: ' + displayCharacters);
 
     return (
-        <div className='character-container'>
-            <h2>{props.name}</h2>
+        <Container danger>
+            <h2 style={changes().h2}>{props.name}</h2>
             <div className='character-info'>
                 <ul>
                     {
                         displayKeys.map((k, idx) => {
-                            return<li key={k}>{k}:{' '}{displayCharacters[idx]}</li>
+                            return <li style={changes().li} key={k}>{k}:{' '}{displayCharacters[idx]}</li>
                         })
                     }
                 </ul>
             </div>
-        </div>
+        </Container>
     )
 }
